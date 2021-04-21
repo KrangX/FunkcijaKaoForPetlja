ef9ea937dbfd5d6fcf7e91cc2f9657b1d18f26e6
Aleksandar Kraguljac <aleksandar.kraguljac@gmail.com>
2021-04-21 22:41:02 +0200

    Add project files.

---
+++ FunkcijaKaoForPetlja/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunkcijaKaoForPetlja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Petlja(5);
+            Console.ReadKey();
+        }
+        public static bool Petlja(int i) {
+            //ovaj deo koda bi se izvrsavao tacno npr 5 puta kao u nekoj for petlji npr
+            //************************************************************************
+            Console.WriteLine(i);
+            //************************************************************************
+            if (i == 1) return true;
+            i--;
+            return Petlja(i);
+        }
+    }
+}
